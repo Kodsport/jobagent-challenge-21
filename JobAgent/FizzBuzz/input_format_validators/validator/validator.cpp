bc439454a5762d87618d87303a426df631c0b644
mattiasutancykeln <mattiasutancykeln@DESKTOP-NNLSKDU.localdomain>
2021-04-10 10:34:37 +0200

    input_validator etc

---
+++ JobAgent/FizzBuzz/input_format_validators/validator/validator.cpp
@@ -0,0 +1,21 @@
+#include "validator.h"
+
+void run(){
+    int n = Int(1, Arg("n")); Space(); int m = Int(1, Arg("m"));
+	Endl();
+
+	for (int i = 0; i < n; i++) {
+		string Lin = Line();
+        int spaces = 0;
+        string sub = "";
+		for (int j = 0, j < Lin.size(), j++){
+            if (Lin[j] == " "){
+                spaces++;
+                assert(sub.size() != 0);
+                sub = "";
+            }
+            else sub += Lin[j];
+        }
+        assert(spaces == m - 1);
+	}
+}
---
+++ JobAgent/FizzBuzz/submissions/wrong_answer/printOnlyOne.py
@@ -0,0 +1,30 @@
+def correct_seq(m):
+    l = []
+    for i in range(1,m+1):
+        s = ""
+        if i % 3 == 0:
+            s += "Fizz"
+        if i % 5 == 0:
+            s += "Buzz"
+        if len(s) == 0:
+            s = str(i)
+        l.append(s)
+    return l
+
+n, m = map(int, input().split())
+corr_seq = correct_seq(m)
+
+best = []
+minerr = 10**9
+for i in range(n):
+    inp = input().split()
+    err = 0
+    for j in range(m):
+        if corr_seq[j] != inp[j]:
+            err += 1
+    
+    if err < minerr:
+        minerr = err
+        best = [str(i+1)]
+
+print(' '.join(best))
