880cb19ab0c24141d1eb7c67e56d2f4c22442ec4
mattiasutancykeln <mattiasutancykeln@DESKTOP-NNLSKDU.localdomain>
2021-04-11 14:57:26 +0200

    Added all required files and data. In need of bugfixes

---
+++ fizzbuzz/data/FizzBuzz_generator.py
@@ -0,0 +1,69 @@
+import random
+import collections
+import sys
+
+def correctSeq(m):
+    l = []
+    for i in range(1,m+1):
+        s = ""
+        if i % 3 == 0:
+            s += "Fizz"
+        if i % 5 == 0:
+            s += "Buzz"
+        if len(s) == 0:
+            s = str(i)
+        l.append(s)
+    return l
+
+def genRandom(n,m, fixed_thresh = 0):
+    correct = correct_seq(m)
+    output = "{} {}".format(n,m) + "\n"
+    thresh = random.uniform(0,1)
+
+    for i in range(n):
+        if !fixed_thresh:
+            thresh = random.uniform(0,1)
+        line = ""
+
+        for j in range(m):
+            if random.uniform(0,1) > thresh:
+                line += correct[j] + " "
+            else:
+                line += random.choice(correct) + " "
+        output += line[:-1] + "\n"
+    
+    print(output)
+
+def allSame(n,m, correct = 1):
+    output = "{} {}".format(n,m) + "\n"
+    if correct:
+        output += "".join([" ".join(correctSeq(m)) + "\n" for _ in range(n)])
+    else:
+        output += (("x "*m)[:-1] + "\n")*n
+    print(output)
+
+def main():
+    random.seed(int(sys.argv[-1]))
+    n = int(cmdlinearg("n"))
+    m = int(cmdlinearg('m'))
+    arg = cmdlinearg('arg')
+
+    if arg == 'r':
+        genRandom(n,m,0)
+    if arg == "rt":
+        genRandom(n,m,1)
+    if arg == "w":
+        allSame(n,m,0)
+    if arg == "c":
+        allSame(n,m,1)
+
+
+
+def cmdlinearg(name):
+    for arg in sys.argv:
+        if arg.startswith(name + "="):
+            return arg.split("=")[1]
+    #return default[name]
+
+if __name__ == "__main__":
+    main()
---
+++ fizzbuzz/data/generator.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+. ../../testdata_tools/gen.sh
+
+use_solution MA.py
+
+compile FizzBuzz_generator.py
+
+samplegroup
+limits n=5 m=15
+sample 1
+sample 2
+sample 3
+
+group group 100
+limits n=1000 m=100
+tc 1
+tc 2
+tc 3
+tc all_correct FizzBuzz_generator n=2 m=2 arg=c
+tc all_correct2 FizzBuzz_generator n=1000 m=100 arg=c
+tc all_correct3 FizzBuzz_generator n=10 m=15 arg=c
+tc all_wrong FizzBuzz_generator n=2 m=2 arg=w
+tc all_wrong2 FizzBuzz_generator n=1000 m=100 arg=w
+tc all_wrong3 FizzBuzz_generator n=10 m=15 arg=w
+tc random0 FizzBuzz_generator n=2 m=2 arg=r
+tc random1 FizzBuzz_generator n=2 m=15 arg=r
+tc random2 FizzBuzz_generator n=2 m=98 arg=r
+tc random3 FizzBuzz_generator n=2 m=99 arg=r
+tc random4 FizzBuzz_generator n=2 m=100 arg=r
+tc random5 FizzBuzz_generator n=15 m=2 arg=r
+tc random6 FizzBuzz_generator n=15 m=15 arg=r
+tc random7 FizzBuzz_generator n=15 m=98 arg=r
+tc random8 FizzBuzz_generator n=15 m=99 arg=r
+tc random9 FizzBuzz_generator n=15 m=100 arg=r
+tc random10 FizzBuzz_generator n=24 m=2 arg=r
+tc random11 FizzBuzz_generator n=24 m=15 arg=r
+tc random12 FizzBuzz_generator n=24 m=98 arg=r
+tc random13 FizzBuzz_generator n=24 m=99 arg=r
+tc random14 FizzBuzz_generator n=24 m=100 arg=r
+tc random15 FizzBuzz_generator n=99 m=2 arg=r
+tc random16 FizzBuzz_generator n=99 m=15 arg=r
+tc random17 FizzBuzz_generator n=99 m=98 arg=r
+tc random18 FizzBuzz_generator n=99 m=99 arg=r
+tc random19 FizzBuzz_generator n=99 m=100 arg=r
+tc random20 FizzBuzz_generator n=1000 m=2 arg=r
+tc random21 FizzBuzz_generator n=1000 m=15 arg=r
+tc random22 FizzBuzz_generator n=1000 m=98 arg=r
+tc random23 FizzBuzz_generator n=1000 m=99 arg=r
+tc random24 FizzBuzz_generator n=1000 m=100 arg=r
+tc randomT0 FizzBuzz_generator n=2 m=2 arg=rt
+tc randomT1 FizzBuzz_generator n=2 m=15 arg=rt
+tc randomT2 FizzBuzz_generator n=2 m=98 arg=rt
+tc randomT3 FizzBuzz_generator n=2 m=99 arg=rt
+tc randomT4 FizzBuzz_generator n=2 m=100 arg=rt
+tc randomT5 FizzBuzz_generator n=15 m=2 arg=rt
+tc randomT6 FizzBuzz_generator n=15 m=15 arg=rt
+tc randomT7 FizzBuzz_generator n=15 m=98 arg=rt
+tc randomT8 FizzBuzz_generator n=15 m=99 arg=rt
+tc randomT9 FizzBuzz_generator n=15 m=100 arg=rt
+tc randomT10 FizzBuzz_generator n=24 m=2 arg=rt
+tc randomT11 FizzBuzz_generator n=24 m=15 arg=rt
+tc randomT12 FizzBuzz_generator n=24 m=98 arg=rt
+tc randomT13 FizzBuzz_generator n=24 m=99 arg=rt
+tc randomT14 FizzBuzz_generator n=24 m=100 arg=rt
+tc randomT15 FizzBuzz_generator n=99 m=2 arg=rt
+tc randomT16 FizzBuzz_generator n=99 m=15 arg=rt
+tc randomT17 FizzBuzz_generator n=99 m=98 arg=rt
+tc randomT18 FizzBuzz_generator n=99 m=99 arg=rt
+tc randomT19 FizzBuzz_generator n=99 m=100 arg=rt
+tc randomT20 FizzBuzz_generator n=1000 m=2 arg=rt
+tc randomT21 FizzBuzz_generator n=1000 m=15 arg=rt
+tc randomT22 FizzBuzz_generator n=1000 m=98 arg=rt
+tc randomT23 FizzBuzz_generator n=1000 m=99 arg=rt
+tc randomT24 FizzBuzz_generator n=1000 m=100 arg=rt
